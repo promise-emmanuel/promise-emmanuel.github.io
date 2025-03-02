c65af13c2d60c30e44723a04071cbc8e79ddca76
Promise Nwabueze Igbojionu <promiseemmanuel@byui.edu>
2025-03-01 17:55:33 -0700

    update

--- week-2/cms/src/app/documents/document-edit/document-edit.component.ts
+++ week-2/cms/src/app/documents/document-edit/document-edit.component.ts
@@ -1,32 +1,103 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
+import { DocumentService } from '../document.service';
 import { Document } from '../document.model';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-document-edit',
   standalone: false,
-  
   templateUrl: './document-edit.component.html',
   styleUrls: ['./document-edit.component.css']
 })
 export class DocumentEditComponent implements OnInit {
-  originalDocument: Document | null = null;
-  document: Document = { id: '', name: '', description: '', url: '', children: [] };
+  id: string;
   editMode: boolean = false;
+  originalDocument: Document | null = null;
+  document: Document = { id: '', name: '', description: '', url: '', children:[]};
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private documentService: DocumentService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.id = params['id'];
+      if (!this.id) {
+        this.editMode = false;
+        return;
+      }
+      this.originalDocument = this.documentService.getDocument(this.id);
+      if (!this.originalDocument) {
+        return;
+      }
+      this.editMode = true;
+      this.document = JSON.parse(JSON.stringify(this.originalDocument));
+    });
+  }
 
   onSubmit(form: NgForm) {
-    // if (form.invalid) {
-      // return;
-    // }
-    // console.log('Form submitted:', form.value);
+    const value = form.value; // get values from form fields
+    const newDocument = new Document(
+      this.id,
+      value.name,
+      value.description,
+      value.url,
+      value.children // assuming children is an array
+    );
+
+    if (this.editMode) {
+      this.documentService.updateDocument(this.originalDocument, newDocument);
+    } else {
+      this.documentService.addDocument(newDocument);
+    }
+    this.router.navigate(['/documents']);
   }
 
   onCancel() {
-    // console.log('Edit cancelled');
+    this.router.navigate(['/documents']);
   }
 }
+
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { NgForm } from '@angular/forms';
+// import { Document } from '../document.model';
+// import { DocumentService } from '../document.service';
+// import { ActivatedRoute, Params, Router } from '@angular/router';
+
+
+// @Component({
+//   selector: 'app-document-edit',
+//   standalone: false,
+  
+//   templateUrl: './document-edit.component.html',
+//   styleUrls: ['./document-edit.component.css']
+// })
+// export class DocumentEditComponent implements OnInit {
+//   originalDocument: Document | null = null;
+//   document: Document = { id: '', name: '', description: '', url: '', children: [] };
+//   editMode: boolean = false;
+
+//   constructor(
+//     private documentService: DocumentService,
+//     private router: Router,
+//     private route: ActivatedRoute
+//   ) {}
+
+//   ngOnInit(): void {}
+
+//   onSubmit(form: NgForm) {
+//     // if (form.invalid) {
+//       // return;
+//     // }
+//     // console.log('Form submitted:', form.value);
+//   }
+
+//   onCancel() {
+//     // console.log('Edit cancelled');
+//   }
+// }
